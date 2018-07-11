de529525abac3b668d8855f9ccaa332f83528c2f
zhanghaidong <Haidong.Zhang@zhan.com>
2018-07-11 20:06:38 +0800

    测试5

--- src/main/java/com/toefl/basic/utils/Test1.java
+++ src/main/java/com/toefl/basic/utils/Test1.java
@@ -6,4 +6,8 @@ package com.toefl.basic.utils;
  * @date 20:00 2018/7/11
  */
 public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("test cherry-pick 1");
+    }
 }
